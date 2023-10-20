c8861c208fa82da35fb51fefc6e951baa2133265
triss-smith <smith.triston@gmail.com>
2023-10-20 07:30:22 -0500

    WIP: refactoring enemy detection

--- horror_game/Assets/Scripts/Enemies/Skulltree Scripts/SkullTreeController.cs
+++ horror_game/Assets/Scripts/Enemies/Skulltree Scripts/SkullTreeController.cs
@@ -11,11 +11,13 @@ public class SkullTreeController : MonoBehaviour
     private Vector3 initialPosition;    // Initial position of the enemy.
     private bool isChasing = false;     // Flag to check if the enemy is chasing.
     private Animator animator;          // Reference to the enemy's animator (if you're using one).
+    private 
     
     // Add any other variables or references as needed.
     
     void Start()
     {
+        
         player = GameObject.FindGameObjectWithTag("Player").transform; // Make sure to tag your player GameObject as "Player."
         initialPosition = transform.position;
         
@@ -27,9 +29,55 @@ public class SkullTreeController : MonoBehaviour
     {
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        // Check if the player is within the detection range.
+        // // Check if the player is within the detection range.
         if (distanceToPlayer <= detectionRange)
-        {
+        // {
+        //     isChasing = true;
+
+        //     // Calculate the direction to the player.
+        //     Vector3 directionToPlayer = (player.position - transform.position).normalized;
+
+        //     // Move the enemy in the direction of the player.
+        //     transform.position += directionToPlayer * moveSpeed * Time.deltaTime;
+
+        //     // Ensure the enemy faces the player.
+        //     if (directionToPlayer.x > 0)
+        //     {
+        //         // If the player is on the right side, flip the enemy to face right.
+        //         transform.localScale = new Vector3(-1, 1, 1);
+        //     }
+        //     else if (directionToPlayer.x < 0)
+        //     {
+        //         // If the player is on the left side, flip the enemy to face left.
+        //         transform.localScale = new Vector3(1, 1, 1);
+        //     }
+            
+        //     // Optional: Update the animator parameters to control animations (if using an animator).
+        //     // animator.SetFloat("Speed", moveSpeed);
+        // }
+        // else
+        // {
+        //     isChasing = false;
+
+        //     // If the enemy is not chasing, it can return to its initial position.
+        //     ReturnToInitialPosition();
+            
+        //     // Optional: Update the animator parameters to control animations (if using an animator).
+        //     // animator.SetFloat("Speed", 0);
+        // }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer <= detectionRange)
+
+        // Check if the collision is with the object you want to interact with
+        if (other.CompareTag("Player")) {
+            
+            // Your code to handle the collision goes here
+            Debug.Log("Collision detected with " + other.name);
+            // You can add your custom actions here
             isChasing = true;
 
             // Calculate the direction to the player.
@@ -39,13 +87,11 @@ public class SkullTreeController : MonoBehaviour
             transform.position += directionToPlayer * moveSpeed * Time.deltaTime;
 
             // Ensure the enemy faces the player.
-            if (directionToPlayer.x > 0)
-            {
+            if (directionToPlayer.x > 0) {
                 // If the player is on the right side, flip the enemy to face right.
                 transform.localScale = new Vector3(-1, 1, 1);
             }
-            else if (directionToPlayer.x < 0)
-            {
+            else if (directionToPlayer.x < 0) {
                 // If the player is on the left side, flip the enemy to face left.
                 transform.localScale = new Vector3(1, 1, 1);
             }
@@ -63,6 +109,7 @@ public class SkullTreeController : MonoBehaviour
             // Optional: Update the animator parameters to control animations (if using an animator).
             // animator.SetFloat("Speed", 0);
         }
+            
     }
 
     void ReturnToInitialPosition()
